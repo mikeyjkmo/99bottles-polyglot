fb3731e7076aef6a5ff55a2376b782df40b80a39
Mikey Mo <mikeyjkmo@gmail.com>
2022-12-20 22:40:24 +0000

    Run all the tests

--- typescript/beer-song.test.ts
+++ typescript/beer-song.test.ts
@@ -52,7 +52,7 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
     expect(Beer.sing(3)).toEqual(expected)
   })
 
-  xit('sings all the verses', () => {
+  it('sings all the verses', () => {
     const song = Beer.sing()
     expect(song).toEqual(`99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
