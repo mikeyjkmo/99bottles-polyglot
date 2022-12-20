09ececf5f93af62092313206c2b44bd07ca8d1f3
Mikey Mo <mikeyjkmo@gmail.com>
2022-12-20 22:20:49 +0000

    Start fixing stuff

--- typescript/beer-song.ts
+++ typescript/beer-song.ts
@@ -1,5 +1,8 @@
 export function verse(verseNumber: number) {
-  return `Verse number ${verseNumber}`
+  return (
+    `${verseNumber} bottles of beer on the wall, ${verseNumber} bottles of beer.\n` +
+    `Take one down and pass it around, ${verseNumber - 1} bottles of beer on the wall.\n`
+  )
 }
 
 export function sing(startingVerse = 99, endingVerse = 0) {
